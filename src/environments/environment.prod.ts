de0cbd0af5d5fbd9244e5db30ed7f1f9e779f372
Roman Manasyan <rmanasyan@gmail.com>
2017-06-15 17:05:26 +0300

    firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBaj3CZScW9UuHgFTMeyVvwJVLAKJ_lBSg',
+    authDomain: 'italianverbs-52cae.firebaseapp.com',
+    databaseURL: 'https://italianverbs-52cae.firebaseio.com',
+    projectId: 'italianverbs-52cae',
+    storageBucket: 'italianverbs-52cae.appspot.com',
+    messagingSenderId: '708734038682'
+  }
 };
