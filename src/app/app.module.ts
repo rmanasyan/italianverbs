acf1e0ada54481ddaf739dc53d1236e65063e533
Roman Manasyan <rmanasyan@gmail.com>
2017-09-29 17:37:00 +0300

    wip: server side rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { environment } from '../environments/environment';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'italianverbs' }),
     FormsModule,
     HttpModule,
     ConjugationModule,
